71712b9732863c48e17a247a63168ca83967a34c
student <i.i.f@mail.ru>
2022-03-09 21:25:06 +0800

    задача 1

--- src/test/skypro/Main.java
+++ src/test/skypro/Main.java
@@ -3,6 +3,17 @@ package test.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // задание 1
+        int i=1;
+        while (i<=10){
+            System.out.print(i+" ");
+            i++;
+        }
+        System.out.println();
+        for (i=10; i>0; i--){
+            System.out.print(i+" ");
+        }
+        System.out.println();
     }
 }
